6d731c715f718bca51704167f492279c28cf46df
Niels Louwes <niels.louwes@and.digital>
2022-04-15 17:11:27 +0200

    Added pages and basic navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Main from "./Main";
+import styled from "styled-components";
+import InfoPage from "./InfoPage";
+
+const Styled = styled.div``;
+
+const Navbar = styled.div`
+  height: 6vh;
+  border: 1px red solid;
+  background: black;
+  color: white;
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello World</h2>
-    </div>
+    <Styled className="App">
+      <Navbar> ARTLE</Navbar>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/info" element={<InfoPage />}></Route>
+      </Routes>
+    </Styled>
   );
 }
 
---
+++ src/InfoPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InfoPage() {
+  return <h2>Great job! Did you know...</h2>;
+}
+
+export default InfoPage;
