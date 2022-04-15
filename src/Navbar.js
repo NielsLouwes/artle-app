a6a3e6a018963289ba43c798760cf447f270ee5b
Niels Louwes <niels.louwes@and.digital>
2022-04-15 17:36:30 +0200

    added navbar icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import StackedLineChartOutlinedIcon from "@mui/icons-material/StackedLineChartOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 const Styled = styled.div`
   height: 6vh;
@@ -7,7 +11,7 @@ const Styled = styled.div`
   background: black;
   color: white;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 `;
 
@@ -15,10 +19,23 @@ const Title = styled.h1`
   font-size: 2.1;
 `;
 
+const IconContainer = styled.div`
+  margin-left: 15px;
+  margin-right: 15px;
+`;
+
 function Navbar() {
   return (
     <Styled>
+      <IconContainer>
+        <InfoOutlinedIcon />
+        <HelpOutlineOutlinedIcon />
+      </IconContainer>
       <Title>Artle</Title>
+      <IconContainer>
+        <SettingsOutlinedIcon />
+        <StackedLineChartOutlinedIcon />
+      </IconContainer>
     </Styled>
   );
 }
