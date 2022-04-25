eeccd7193fe2db7d69442b6f130af24e08a77009
Niels Louwes <niels.louwes@and.digital>
2022-04-25 22:56:28 +0200

    setting up infopage

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function InfoPage() {
+  const retrieveImage = async () => {
+    try {
+      const response = await axios(
+        `https://api.artic.edu/api/v1/artworks/27991?fields=id,title,image_id/`
+      );
+      const json = await response.data.data;
+      console.log(json);
+      const imageId = await json.image_id;
+      // setImage(imageId);
+      console.log(imageId);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    retrieveImage();
+  }, []);
+
   return <h2>Great job! Did you know...</h2>;
 }
 
--- src/completePage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function CompletePage() {
-  return <></>;
-}
-
-export default CompletePage;
