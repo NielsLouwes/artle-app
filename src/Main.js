b6c7ab37db3e02d13fda5b28c753bf66408432e3
Niels Louwes <niels.louwes@and.digital>
2022-05-01 16:45:56 +0200

    sorted out paintingInfo

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 // import axios from 'axios';
 import Box from '@mui/material/Box';
@@ -28,28 +29,47 @@ const Image = styled.img`
 function Main({ art }) {
   // const [image, setImage] = useState('');
   // const [data, setData] = useState(null);
+  const [paintingData, setPaintingData] = useState([]);
 
   //randomize
   //in the URL after artworks/ there is the number for the artworks
   // randomize that with a function
 
   // Put the information I need into a piece of state as an object to share to the next page when win/loss
-  // const state = [
-    {
-      artist: data.artist_title,
-      title: data.title,
-      classification: data.classification_title,
-      
+  // const paintingData = [
+  //   {
+  //     artist: data.artist_title,
+  //     title: data.title,
+  //     classification: data.classification_title,
+  //   }
+  // ]
 
+  // need to split the name and only grab last name
+  //compare that with input of the user
 
-    }
-  ]
+  // const guessName = (input) => {
+  //   if (input === data)
+  // }
   // eslint-disable-next-line no-unused-vars
   const imageInformation = () => {
     fetch(`https://api.artic.edu/api/v1/artworks/27980`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        const artistTitle = data.data.artist_title;
+        const paintingTitle = data.data.title;
+        const classificationType = data.data.classification_titles[0];
+
+        console.log(paintingTitle);
+        console.log(classificationType);
+        console.log(artistTitle);
+
+        setPaintingData({
+          artist: artistTitle,
+          title: paintingTitle,
+          classification: classificationType
+        });
+        console.log(paintingData);
       });
   };
 
@@ -70,6 +90,7 @@ function Main({ art }) {
         noValidate
         autoComplete="off">
         <TextField id="outlined-basic" label="Guess last name" variant="outlined" />
+        <Link to="/info">See Info</Link>
       </Box>
     </Styled>
   );
