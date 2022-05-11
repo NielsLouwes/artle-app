da63f7eacada471efe5a09bed31272e52480f666
Niels Louwes <niels.louwes@and.digital>
2022-05-11 08:49:27 +0200

    Adds filtered collection results from API

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const Styled = styled.div``;
 function App() {
   const [art, setArt] = useState([]);
   const [paintingData, setPaintingData] = useState([]);
+  const [collection, setCollection] = useState([]);
   const [loading, setLoading] = useState(false);
 
   // const baseArtUrl = `https://www.artic.edu/iiif/2/`;
@@ -29,35 +30,30 @@ function App() {
 
   // const randomNumber = getRandomInt(29000);
 
-  const getArtData = () => {
+  //To get Rijks paint data
+  // 1. Do API call for collection
+  // 2. put collection in collection state
+  // 3. spread that collection state and filter it to exclude "anonomous painters"
+  // 4. Grab a random objectNumber and use that in another API call to directly for that painting
+
+  const getCollectionData = () => {
     setLoading(true);
-    fetch(`https://www.rijksmuseum.nl/api/en/collection/SK-C-5?key=f1nLs8AG`, {
-      headers: {
-        'Content-type': 'application/json',
-        Accept: 'application/json'
+    fetch(
+      `https://www.rijksmuseum.nl/api/en/collection?key=f1nLs8AG&material=oil%20paint%20(paint)&material=canvas&yearfrom=1550&yearto=1900&st=Objects&ii=0&ps=100`,
+      {
+        headers: {
+          'Content-type': 'application/json',
+          Accept: 'application/json'
+        }
       }
-    })
+    )
       .then(function (response) {
-        console.log(response);
         return response.json();
       })
       .then(function (data) {
-        const paintingYear = data.artObject?.dating.sortingDate;
-        const artistName = data.artObject?.principalMakers[0].name;
-        const paintingTitle = data.artObject?.title;
-        const paintingImageLink = data.artObject?.webImage.url;
-        const paintingDescription = data.artObject?.label.description;
-        const physicalMedium = data.artObject?.physicalMedium;
-        console.log(data);
-        setArt(paintingImageLink);
-        setPaintingData({
-          artist: artistName,
-          title: paintingTitle,
-          medium: physicalMedium,
-          year: paintingYear,
-          description: paintingDescription,
-          image: paintingImageLink
-        });
+        console.log(data.artObjects);
+        console.log(data.artObjects[0].objectNumber); // finding specific paintingID
+        setCollection(data.artObjects);
       })
       .catch((error) => {
         if (error.response) {
@@ -70,11 +66,60 @@ function App() {
       });
   };
 
-  useEffect(() => {
-    getArtData();
-  }, []);
+  //take the collection of top 100 results and filter to exclude anonymous
+  console.log(collection);
+  const filteredCollection = collection.filter(
+    (element) => element.principalOrFirstMaker !== 'anonymous'
+  );
+  console.log(filteredCollection);
 
-  console.log(art);
+  //  const getArtData = () => {
+  //   setLoading(true);
+  //   fetch(
+  //     `https://www.rijksmuseum.nl/api/en/collection?key=f1nLs8AG&material=oil%20paint%20(paint)&material=canvas&yearfrom=1550&yearto=1900&st=Objects&ii=0&ps=100`,
+  //     {
+  //       headers: {
+  //         'Content-type': 'application/json',
+  //         Accept: 'application/json'
+  //       }
+  //     }
+  //   )
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       console.log(data);
+  //       const paintingYear = data.artObject?.dating.sortingDate;
+  //       const artistName = data.artObject?.principalMakers[0].name;
+  //       const paintingTitle = data.artObject?.title;
+  //       const paintingImageLink = data.artObject?.webImage.url;
+  //       const paintingDescription = data.artObject?.label.description;
+  //       const physicalMedium = data.artObject?.physicalMedium;
+
+  //       setArt(paintingImageLink);
+  //       setPaintingData({
+  //         artist: artistName,
+  //         title: paintingTitle,
+  //         medium: physicalMedium,
+  //         year: paintingYear,
+  //         description: paintingDescription,
+  //         image: paintingImageLink
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       if (error.response) {
+  //         console.log(error.response.data);
+  //       }
+  //       console.error('Request failed', error);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // };
+
+  useEffect(() => {
+    getCollectionData();
+  }, []);
 
   return (
     <Styled className="App">
