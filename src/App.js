06664a5faecc191f42e138ae3f3f3c01cfc3ca75
Niels Louwes <niels.louwes@and.digital>
2022-05-13 09:00:35 +0200

    Start of refactor API calls and random function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import InfoPage from './InfoPage';
 import Navbar from './Navbar';
 
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Styled = styled.div``;
 
@@ -16,23 +17,131 @@ function App() {
   const [collection, setCollection] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // const baseArtUrl = `https://www.artic.edu/iiif/2/`;
-  // const rijksArtApi = 'f1nLs8AG';
   // const exampleRijksCall = 'https://www.rijksmuseum.nl/api/en/collection/SK-C-5?key=f1nLs8AG';
-
-  //add this randomize as the imageID , append to baseArtUrl
-  // const randomizePaintingId = Math.round(Math.random() * 29000);
-  // const getRandomInt = (max) => {
-  //   return Math.floor(Math.random() * max);
-  // };
-
-  // const randomNumber = getRandomInt(29000);
 
   //To get Rijks paint data
   // 1. Do API call for collection
   // 2. put collection in collection state
   // 3. spread that collection state and filter it to exclude "anonomous painters"
   // 4. Grab a random objectNumber and use that in another API call to directly for that painting
+
+  // const getCollectionData = () => {
+  //   setLoading(true);
+  //   fetch(
+  //     `https://www.rijksmuseum.nl/api/en/collection?key=f1nLs8AG&material=oil%20paint%20(paint)&material=canvas&yearfrom=1550&yearto=1900&st=Objects&ii=0&ps=100`,
+  //     {
+  //       headers: {
+  //         'Content-type': 'application/json',
+  //         Accept: 'application/json'
+  //       }
+  //     }
+  //   )
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       console.log(data.artObjects);
+  //       console.log(data.artObjects[0].objectNumber); // finding specific paintingID
+  //       setCollection(data.artObjects);
+  //     })
+  //     .catch((error) => {
+  //       if (error.response) {
+  //         console.log(error.response.data);
+  //       }
+  //       console.error('Request failed', error);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // };
+
+  // const retrieveRandomPaintingIdFromCollection = async () => {
+  //   //take the collection of top 100 results and filter to exclude anonymous
+  //   const filteredCollection = await collection.filter(
+  //     (element) => element.principalOrFirstMaker !== 'anonymous'
+  //   );
+  //   console.log(collection);
+  //   console.log(filteredCollection); // 98 results
+
+  //   // grab the keys from the object
+  //   const filteredCollectionKeys = await Object.keys(filteredCollection);
+  //   console.log(filteredCollectionKeys);
+
+  //   //generate random index based on number of keys
+  //   const randIndex = await Math.floor(Math.random() * filteredCollectionKeys.length);
+  //   console.log(randIndex);
+
+  //   //select key from the array of keys using the random index
+  //   const randomKey = await filteredCollectionKeys[randIndex];
+  //   console.log(randomKey);
+
+  //   //get the painting ID from the key
+  //   const paintingReferenceNumber = await filteredCollection[randomKey];
+  //   console.log(paintingReferenceNumber);
+
+  //   const paintingID = await paintingReferenceNumber.objectNumber;
+  //   setArtKey(paintingID);
+  //   console.log(paintingID); //returns PaintingID to be used for another API call to get image and specific painting data
+  // };
+
+  // const getSpecificArtData = async () => {
+  //   setLoading(true);
+  //   await fetch(`https://www.rijksmuseum.nl/api/en/collection/${artKey}?key=f1nLs8AG`, {
+  //     headers: {
+  //       'Content-type': 'application/json',
+  //       Accept: 'application/json'
+  //     }
+  //   })
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       console.log(data);
+  //       const paintingYear = data.artObject?.dating.sortingDate;
+  //       const artistName = data.artObject?.principalMakers[0].name;
+  //       const paintingTitle = data.artObject?.title;
+  //       const paintingImageLink = data.artObject?.webImage.url;
+  //       const paintingDescription = data.artObject?.label.description;
+  //       const physicalMedium = data.artObject?.physicalMedium;
+
+  //       console.log(paintingYear);
+  //       console.log(artistName);
+  //       console.log(paintingDescription);
+
+  //       setPaintingData({
+  //         artist: artistName,
+  //         title: paintingTitle,
+  //         medium: physicalMedium,
+  //         year: paintingYear,
+  //         description: paintingDescription,
+  //         image: paintingImageLink
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       if (error.response) {
+  //         console.log(error.response.data);
+  //       }
+  //       console.error('Request failed', error);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // };
+
+  //2nd attempt to chain different API calls and fix errors in console
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.rijksmuseum.nl/api/en/collection?key=f1nLs8AG&material=oil%20paint%20(paint)&material=canvas&yearfrom=1550&yearto=1900&st=Objects&ii=0&ps=100`
+      )
+      .then((response) => {
+        setCollection(response.data.artObjects);
+        console.log(response);
+        console.log(collection);
+        console.log(response.data.artObjects[0].objectNumber); // finding specific paintingID
+      });
+  }, []);
 
   const getCollectionData = () => {
     setLoading(true);
@@ -64,18 +173,6 @@ function App() {
       });
   };
 
-  useEffect(() => {
-    getCollectionData();
-  }, []);
-
-  useEffect(() => {
-    retrieveRandomPaintingIdFromCollection();
-  }, [collection]);
-
-  useEffect(() => {
-    getSpecificArtData();
-  }, [artKey]);
-
   const retrieveRandomPaintingIdFromCollection = async () => {
     //take the collection of top 100 results and filter to exclude anonymous
     const filteredCollection = await collection.filter(
@@ -105,9 +202,9 @@ function App() {
     console.log(paintingID); //returns PaintingID to be used for another API call to get image and specific painting data
   };
 
-  const getSpecificArtData = () => {
+  const getSpecificArtData = async () => {
     setLoading(true);
-    fetch(`https://www.rijksmuseum.nl/api/en/collection/${artKey}?key=f1nLs8AG`, {
+    await fetch(`https://www.rijksmuseum.nl/api/en/collection/${artKey}?key=f1nLs8AG`, {
       headers: {
         'Content-type': 'application/json',
         Accept: 'application/json'
@@ -149,7 +246,19 @@ function App() {
       });
   };
 
-  console.log(paintingData);
+  // useEffect(() => {
+  //   getCollectionData();
+  // }, []);
+
+  // useEffect(() => {
+  //   retrieveRandomPaintingIdFromCollection();
+  // }, [collection]);
+
+  // useEffect(() => {
+  //   getSpecificArtData();
+  // }, [artKey]);
+
+  // console.log('', paintingData);
 
   return (
     <Styled className="App">
